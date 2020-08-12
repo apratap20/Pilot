c6300d1f56350383abb230bb3dd5bc3145abc7ff
apratap20 <apratap20@gmail.com>
2020-08-12 10:42:28 +0530

    Updated the Java File

--- First.java
+++ First.java
@@ -1,5 +1,15 @@
-public class First{
-	public static void main(String args[]{
-		System.out.println("Hello World");
-		}
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.*;  
+public class First extends HttpServlet{
+private static final long serialVersionUID = -9155760945094023888L;
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		int k=i+j;
+		PrintWriter out=res.getWriter();
+		out.println("The Result is "+k);
+		
+	}			
 }
